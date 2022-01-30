009405a233172f1e3638ebbf09919a87e7e82273
Julia Bergman <julia.bergman@helsinki.fi>
2022-01-30 18:25:40 +0200

    Tee 1.7

--- osa_1/unicafe/src/App.js
+++ osa_1/unicafe/src/App.js
@@ -4,15 +4,32 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Statistic = ({category, value}) => (
+const Count = ({category, value}) => (
   <p>{category} {value}</p>
 )
+
+const Average = ({average, total}) => {
+  if (total===0) {
+    return <p>average 0</p>
+  }
+  return <p>average {average}</p>
+}
+
+const Positive = ({positive}) => {
+  if (isNaN(positive)) {
+    return <p>positive 0 %</p>
+  }
+  return <p>positive {positive} %</p>
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1)/all
+  const positive = good / all * 100
 
   return (
     <div>
@@ -23,9 +40,12 @@ const App = () => {
         <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
         <Button handleClick={() => setBad(bad + 1)} text='bad' />
         <h3>Statistics:</h3>
-        <Statistic category='good' value={good}></Statistic>
-        <Statistic category='neutral' value={neutral}></Statistic>
-        <Statistic category='bad' value={bad}></Statistic>
+        <Count category='good' value={good} />
+        <Count category='neutral' value={neutral} />
+        <Count category='bad' value={bad} />
+        <p>all {all}</p>
+        <Average average={average} total={all}/>
+        <Positive positive={positive}/>
       </div>
     </div>
   )
