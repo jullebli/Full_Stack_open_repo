37b7f5e40d74b7fc3b891bc8bca9a0886fe67102
Julia Bergman <julia.bergman@helsinki.fi>
2022-02-01 23:02:00 +0200

    Tee 1.10

--- osa_1/unicafe/src/App.js
+++ osa_1/unicafe/src/App.js
@@ -4,8 +4,8 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Count = ({category, value}) => (
-  <p>{category} {value}</p>
+const StatisticLine = ({text, value}) => (
+  <p>{text} {value}</p>
 )
 
 const Average = ({average, total}) => {
@@ -36,11 +36,9 @@ const Statistics = (props) => {
   const average = (good * 1 + neutral * 0 + bad * -1)/all
   const positive = good / all * 100
 
-  
-
-  return <div><Count category='good' value={good} />
-  <Count category='neutral' value={neutral} />
-  <Count category='bad' value={bad} /><p>all {all}</p>
+  return <div><StatisticLine text="good" value={good} />
+  <StatisticLine text="neutral" value={neutral} />
+  <StatisticLine text='bad' value={bad} /><p>all {all}</p>
   <Average average={average} total={all} /><Positive positive={positive} /></div>
 }
 
@@ -49,7 +47,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
 
   return (
     <div>
