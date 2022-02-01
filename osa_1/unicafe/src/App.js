73d86c5ba2bddee40530da3bf2bd163dadc7e9d6
Julia Bergman <julia.bergman@helsinki.fi>
2022-02-02 00:33:19 +0200

    Tee 1.11

--- osa_1/unicafe/src/App.js
+++ osa_1/unicafe/src/App.js
@@ -5,28 +5,17 @@ const Button = ({handleClick, text}) => (
 )
 
 const StatisticLine = ({text, value}) => (
-  <p>{text} {value}</p>
+  <tr><td>{text}</td><td>{value}</td></tr>
 )
-
-const Average = ({average, total}) => {
-  if (total===0) {
-    return <p>average 0</p>
-  }
-  return <p>average {average}</p>
-}
 
 const Positive = ({positive}) => {
   if (isNaN(positive)) {
-    return <p>positive 0 %</p>
+    return <tr><td>positive</td><td>0 %</td></tr>
   }
-  return <p>positive {positive} %</p>
+  return <tr><td>positive</td><td>{positive} %</td></tr>
 }
 
-const Statistics = (props) => {
-  const good = props.good
-  const neutral = props.neutral
-  const bad = props.bad
-
+const Statistics = ({good, neutral, bad}) => {
   const all = good + neutral + bad
 
   if (all===0) {
@@ -36,10 +25,18 @@ const Statistics = (props) => {
   const average = (good * 1 + neutral * 0 + bad * -1)/all
   const positive = good / all * 100
 
-  return <div><StatisticLine text="good" value={good} />
-  <StatisticLine text="neutral" value={neutral} />
-  <StatisticLine text='bad' value={bad} /><p>all {all}</p>
-  <Average average={average} total={all} /><Positive positive={positive} /></div>
+  return (
+  <table>
+    <tbody>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={all} />
+      <StatisticLine text='average' value={average} />
+      <StatisticLine text='positive' value={positive + " %"} />
+    </tbody>
+  </table>
+  )
 }
 
 const App = () => {
@@ -49,18 +46,17 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      <div>
-        <h1>Unicafe</h1>
-        <h2>Give feedback:</h2>
-        <Button handleClick={() => setGood(good + 1)} text='good' />
-        <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-        <Button handleClick={() => setBad(bad + 1)} text='bad' />
-        <h3>Statistics:</h3>
-        <Statistics good={good} neutral={neutral} bad={bad}/>
-      </div>
-    </div>
+    <>
+      <h1>Unicafe</h1>
+      <h2>Give feedback:</h2>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <h3>Statistics:</h3>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </>
   )
 }
 
 export default App
+// or <StatisticLine text='positive' value={`${positive} %`} />
