bc53b8af7c9e830f70f9f82b55087d584696bc5e
Julia Bergman <julia.bergman@helsinki.fi>
2022-02-20 17:59:36 +0200

    Tee 2.15

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -27,7 +27,11 @@ const App = () => {
 
     } else {
       const newPerson = { name: newName, number: newNumber }
-      setPersons(persons.concat(newPerson))
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then(response => {
+          setPersons(persons.concat(newPerson))
+        })
     }
 
     setNewName('')
