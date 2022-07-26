990f166b4da4116954458218e9676d0231c8c087
Julia Bergman <julia.bergman@helsinki.fi>
2022-07-26 17:46:10 +0300

    Tee 4.8

---
+++ osa_4/tests/blog_api.test.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are six blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(6)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
