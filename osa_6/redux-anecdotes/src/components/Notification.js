bbcc8d945f6fda0eaf86e0b04a5f270da1b090c2
Julia Bergman <julia.bergman@helsinki.fi>
2022-08-11 17:37:41 +0300

    Tee 6.19 eli connect-funktio harjoittelua

--- osa_6/redux-anecdotes/src/components/Notification.js
+++ osa_6/redux-anecdotes/src/components/Notification.js
@@ -1,9 +1,10 @@
-import { useSelector } from 'react-redux'
+//import { useSelector } from 'react-redux'
+import { connect } from "react-redux"
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  //const notification = useSelector(state => state.notification)
 
-  if (!notification) {
+  if (!props.notification) {
     return null
   }
 
@@ -14,9 +15,18 @@ const Notification = () => {
   }
   return (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotification
+//export default Notification
