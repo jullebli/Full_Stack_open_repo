fb72d929d5627debe2eb9423801ca19746ae3155
Julia Bergman <julia.bergman@helsinki.fi>
2022-08-11 14:04:34 +0300

    Tee 6.13

--- osa_6/redux-anecdotes/src/index.js
+++ osa_6/redux-anecdotes/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import anecdoteService from './services/anecdotes'
 import store from './components/store'
 import App from './App'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
+anecdoteService.getAll().then(anecdotes =>
+  store.dispatch(setAnecdotes(anecdotes))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
